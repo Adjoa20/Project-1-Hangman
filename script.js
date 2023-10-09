5d89624f8243e6dc00e467ec13d95d5a0d1c882b
agathaa <aamoako1120@gmail.com>
2023-10-09 19:27:53 -0400

    user input

---
+++ script.js
@@ -0,0 +1,23 @@
+// alert("JS Linked")
+
+// ANSWERS TO THE HINTS 
+const fashionWords = ['Coco Chanel', 'Namoi Campbell', 'Ralph Lauren', 'Bandana', 'Calvin Klien', 'Levi Strauss']
+const musicWords = ['New Orleans', 'Swing', 'Jay-Z' ]
+const moviesWords = []
+const tvWords  = []
+const celebsWord = []
+
+// FUNCTION TO START THE GAME 
+function startGame(){
+
+}
+
+// FUNCTION TO MAKE THE BUTTONS CLICK - USER SHOULD BE ABLE TO SELECT THE LETTER 
+
+//FUNCTION TO CREATE THE HINTS 
+
+// THIS THE METHOD TO CREATE THE HANGMAN 
+
+// CREATE THE ARRAY TO LOOP THE LETTERS 
+
+// CREATE THE ARRAY TO LOOP THROUGH THE ANSWERS AT RANDOM 
