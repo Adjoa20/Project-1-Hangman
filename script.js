29ae65e150b5ac388612afc9393149d3e53c2eff
agathaa <aamoako1120@gmail.com>
2023-10-09 22:36:55 -0400

    hints added

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 // alert("JS Linked")
 
+// 
+
 // ANSWERS TO THE HINTS 
 const fashionWords = ['Coco Chanel', 'Namoi Campbell', 'Ralph Lauren', 'Bandana', 'Calvin Klien', 'Levi Strauss']
-const musicWords = ['New Orleans', 'Swing', 'Jay-Z' ]
-const moviesWords = []
-const tvWords  = []
-const celebsWord = []
+const musicWords = ['New Orleans', 'Swing', 'Jay-Z', 'Beyonce', 'Kendrick Lamar', 'Beastie Boys', 'Redman' ]
+const moviesWords = ['American Psycho', 'Paranormal Activiy', 'Midsommar', 'Dawn Of The Dead', 'Psycho', 'Alien']
+const tvWords  = ['Twin Peaks', 'Bobs Burgers', 'Shamless', 'Futurama', 'Parks & Recreation', '30 Rock']
+const celebsWord = ['Justin Bieber', 'Michael Jackson', 'Nicki Minaj', 'Drake', 'Elon Musk', 'Eminem']
+
+let words = words[Math.floor(Math.random()* words.length)]
 
 // FUNCTION TO START THE GAME 
 function startGame(){
@@ -20,4 +24,18 @@ function startGame(){
 
 // CREATE THE ARRAY TO LOOP THE LETTERS 
 
-// CREATE THE ARRAY TO LOOP THROUGH THE ANSWERS AT RANDOM 
+// CREATE THE ARRAY TO LOOP THROUGH THE ANSWERS AT RANDOM
+let answers = []
+for (let i = 0; i < words.length; i++){
+    answers[i] = '_'
+} 
+
+let letLeftOverLetters = words.length
+
+// FUNCTION TO INPUT USER NAME
+function submitFunction(){
+   let userInput = document.querySelector("#userInput")
+   let message = document.querySelector("#message")
+
+    message.innerHTML = "Welcome To Hangman " + userInput.value
+}
