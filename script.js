c21214d2335e88cb2dcc3f0dacea99ac890dc57b
agathaa <aamoako1120@gmail.com>
2023-10-15 17:13:58 -0400

    added in event listeners

--- script.js
+++ script.js
@@ -41,6 +41,52 @@ function submitFunction(){
     message.innerHTML = 'Welcome To Hangman ' + userInput.value
 }
 
+// ADD IN FUNCTIONS FOR THE BUTTONS 
+function select(){
+    console.log('clicked')
+}
+function select1(){
+    console.log('clicked')
+}
+function select2(){
+    console.log('clicked')
+}
+function select3(){
+    console.log('clicked')
+}
+function select4(){
+    console.log('clicked')
+}
+
+
+// ADDED IN EVENT LISTENERS
+const fashionButton = document.getElementById('fashion-button')
+fashionButton.addEventListener('click', () => {
+    select()
+    alert('You Selected This Category')
+})
+const musicButton = document.getElementById('music-button')
+musicButton.addEventListener('click', () => {
+    select1()
+    alert('You Selected This Category')
+})
+const moviesButton = document.getElementById('movies-button')
+moviesButton.addEventListener('click', () => {
+    select2()
+    alert('You Selected This Category')
+})
+const tvButton = document.getElementById('tv-button')
+tvButton.addEventListener('click', () => {
+    select3()
+    alert('You Selected This Category')
+})
+const celebritiesButton = document.getElementById('celebrities-button')
+celebritiesButton.addEventListener('click', () => {
+    select4()
+    alert('You Selected This Category')
+})
+
+
 
 // FUNCTION WHEN USER SELECTS TO PLAY AGAIN 
 const newGame = () => {
