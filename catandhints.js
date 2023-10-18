4dbda4ab23d0475ae0af2adf7e15e190c8dc3487
agathaa <aamoako1120@gmail.com>
2023-10-18 00:01:47 -0400

    added in the function for the answer and hints

--- catandhints.js
+++ catandhints.js
@@ -1,21 +1,114 @@
-const fashionList = 
+
+const musicList = [ 
+    {
+        answer: 'New Orleans',
+        hint: 'Jazz originated in which city in the United States?',
+    },
+    {
+        answer: 'Swing',
+        hint: 'What is the term used to describe the rhythmic pattern in jazz that emphasizes the offbeats?',
+    },
+    {
+        answer: 'Jay-Z',
+        hint: 'The Black Album was released by which artist?',
+    },
+    {
+        answer: 'Beyonce',
+        hint: 'This R&B Diva was born in the city of Houston and apart of the 1990s girl group ',
+    },
+    {
+        answer: 'Kendrick Lamar',
+        hint: 'Which hip-hop artist is the first to have won a Pulitzer Prize?',
+    },
+    {
+        answer: 'Beastie Boys',
+        hint: 'Hello Nasty was an album by which hip-hop band formed in 1981?',
+    },
+    {
+        answer: 'Redman',
+        hint: 'Which of these was not an official part of Wu Tang Clan/s nine members?',
+    }
+]
+
+movieList = [
+    {
+        answer: 'American Psycho',
+        hint: 'In what movie does someone get hit in the face with an ax while listening to “Hip to Be Square”?',
+    },
+    {
+        answer: 'Paranormal Activiy',
+        hint: 'Which horror movie was filmed in just 7 days?',
+    },
+    {
+        answer: 'Midsommar',
+        hint: 'In which horror film do a group of American students get involved in a Swedish cult?',
+    },
+    {
+        answer: 'Dawn Of The Dead',
+        hint: 'Which George A. Romero movie takes places in a mall?',
+    },
+    {
+        answer: 'Psycho',
+        hint: 'What horror movie features a serial killer named Norman Bates?',
+    },
+    {
+        answer: 'Alien',
+        hint: 'Which sci fi horror film had the tagline In space no one can hear you scream?',
+    },
+]
+
+const tvShowsList = [
+    {
+        answer: 'Twin Peaks',
+        hint: 'An FBI agent investigates the death of Laura Palmer in a bizarre small town.',
+    },
+    {
+        answer: 'Bobs Burgers',
+        hint: 'An unusual family runs a restaurant near the Wonder Wharf amusement park.',
+    },
+    {
+        answer: 'Shamless',
+        hint: 'Alcoholic Frank Gallagher tries to cope with his six children.',
+    },
+    {
+        answer: 'Futurama',
+        hint: 'A delivery boy from the 21st century is frozen and wakes up in the 31st century.',
+    },
+    {
+        answer: 'Parks & Recreation',
+        hint: 'Amy Poehler plays a public official in the fictional town of Pawnee, Indiana.',
+    },
+    {
+        answer: '30 Rock',
+        hint: 'Alec Baldwin won the Emmy for Best Actor Comedy Series twice for which TV show?',
+    },
+]
+
+const celebsList = [
+    {
+        answer: 'Justin Bieber',
+        hint: 'This artist 33 Guiness World Records',
+    },
+    {
+        answer: 'Michael Jackson',
+        hint: 'Won 8 Grammy Awards in one year',
+    },
+    {
+        answer: 'Nicki Minaj',
+        hint: 'Won Best Female Hip-Hop artist at the BET awards for 7 years in a row',
+    },
+    {
+        answer: 'Drake',
+        hint: 'DeGrassi The Next Generstion',
+    },
+    {
+        answer: 'Elon Musk',
+        hint: 'Pretoria, South Africa',
+    },
+    {
+        answer: 'Eminem',
+        hint: 'Won an Academy Award for Best Original Song',
+    }
+]
 
 
-
-
-
-categories = {
-    Fashion: ['Coco Chanel', 'Namoi Campbell', 'Ralph Lauren', 'Bandana', 'Calvin Klien', 'Levi Strauss'],
-    Music: ['New Orleans', 'Swing', 'Jay-Z', 'Beyonce', 'Kendrick Lamar', 'Beastie Boys', 'Redman' ],
-    Movies: ['American Psycho', 'Paranormal Activiy', 'Midsommar', 'Dawn Of The Dead', 'Psycho', 'Alien'],
-    TVShows: ['Twin Peaks', 'Bobs Burgers', 'Shamless', 'Futurama', 'Parks & Recreation', '30 Rock'],
-    Celebs: ['Justin Bieber', 'Michael Jackson', 'Nicki Minaj', 'Drake', 'Elon Musk', 'Eminem']
-}
-
-hints = {
-    fashionHint:['Which designer created the little black dress?','She became the first black model to appear on the cover of French Vogue.','Which fashion designer has a brand called Rugby?',' What item of clothing was Axel Rose from Guns N Roses known for donning?','The world/s biggest selling brand of men/s underwear is?','Who invented jeans?'],
-    musicHint:['Jazz originated in which city in the United States?','What is the term used to describe the rhythmic pattern in jazz that emphasizes the offbeats?','The Black Album was released by which artist?','This R&B Diva was born in the city of Houston and apart of the 1990s girl group ','Which hip-hop artist is the first to have won a Pulitzer Prize?','Hello Nasty was an album by which hip-hop band formed in 1981?','Which of these was not an official part of Wu Tang Clan/s nine members?'],
-    moviesHint:['In what movie does someone get hit in the face with an ax while listening to “Hip to Be Square”?','Which horror movie was filmed in just 7 days?','In which horror film do a group of American students get involved in a Swedish cult?','Which George A. Romero movie takes places in a mall?','What horror movie features a serial killer named Norman Bates?','Which sci fi horror film had the tagline In space no one can hear you scream?'],
-    tvHint:['An FBI agent investigates the death of Laura Palmer in a bizarre small town.','An unusual family runs a restaurant near the Wonder Wharf amusement park.','Alcoholic Frank Gallagher tries to cope with his six children.','A delivery boy from the 21st century is frozen and wakes up in the 31st century.','Amy Poehler plays a public official in the fictional town of Pawnee, Indiana.','Alec Baldwin won the Emmy for Best Actor Comedy Series twice for which TV show?'],
-    celebsHint:['This artist 33 Guiness World Records','Won 8 Grammy Awards in one year','Won Best Female Hip-Hop artist at the BET awards for 7 years in a row','DeGrassi The Next Generstion','Pretoria, South Africa']
-}
--- fashionscript.js
+++ fashionscript.js
@@ -1,7 +1,39 @@
 // FUNCTION FOR THE ALPHABET TO WORK 
 const alphabetKey = document.querySelector('.alphabet-key')
+
 for (let i = 65; i <= 90; i++){
     const button = document.createElement('button')
     button.innerText = String.fromCharCode(i)
     alphabetKey.appendChild(button)
 }
+
+const fashionList = [
+    { answer: 'Coco Chanel',
+      hint: 'Which designer created the little black dress?',
+    },
+    { answer: 'Namoi Campbell',
+      hint: 'She became the first black model to appear on the cover of French Vogue.',
+    },
+    { answer: 'Ralph Lauren',
+      hint: 'Which fashion designer has a brand called Rugby?',
+    },
+    { answer: 'Bandana',
+      hint: 'What item of clothing was Axel Rose from Guns N Roses known for donning?',
+    },
+    { answer: 'Calvin Klien',
+      hint: 'The world/s biggest selling brand of men/s underwear is?',
+    },
+    { answer: 'Levi Strauss',
+      hint: 'Who invented jeans?',
+    },
+]
+
+// FASHION ANSWERS AND HINTS 
+const fashionAns = () => {
+    const { answer, hint} = fashionList[Math.floor(Math.random() * fashionList.length)]
+    console.log(answer, hint)
+}
+
+fashionAns()
+
+
