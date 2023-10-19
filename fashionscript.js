0eaffd0c2f53da15a8723badeb91df35dbf3336c
agathaa <aamoako1120@gmail.com>
2023-10-19 19:18:20 -0400

    started project all over

--- fashionscript.js
+++ fashionscript.js
@@ -1,64 +1,16 @@
-// NEED TO COUNT THE NUMBER OF WINS
-let correctAnswer = ''
-let selectHint= ''
-let userLives= 10
-let input = []
-let checkWin = []
+// COUNTING THE USER'S LIVES
+let correctAnswer= ''
+let userLives = 10
+let winCount = 0
 
-// FUNCTION FOR THE ALPHABET TO WORK / ADDED IN LISTENERS
-const alphabetKey = document.querySelector('.alphabet-key')
+// DEFINING THE ELEMENTS 
+const alphabetKey = document.getElementById('alphabetKey')
 const displayLetter = document.getElementById('display-letter')
-
-for (let i = 65; i <= 90; i++){
-    const button = document.createElement('button')
-    button.innerText = String.fromCharCode(i)
-    alphabetKey.appendChild(button)
-
-    button.addEventListener('click', (event) => {
-        const clickedButton = event.target
-        const clickedLetter = clickedButton.innerText
-        console.log('Clicked')
-
-        displayLetter.innerText = '' +  clickedLetter
-    })
-}
-
-const fashionList = [
-    { answer: 'Coco Chanel',
-      hint: 'Which designer created the little black dress?',
-    },
-    { answer: 'Namoi Campbell',
-      hint: 'She became the first black model to appear on the cover of French Vogue.',
-    },
-    { answer: 'Ralph Lauren',
-      hint: 'Which fashion designer has a brand called Rugby?',
-    },
-    { answer: 'Bandana',
-      hint: 'What item of clothing was Axel Rose from Guns N Roses known for donning?',
-    },
-    { answer: 'Calvin Klien',
-      hint: 'The worlds biggest selling brand of men/s underwear is?',
-    },
-    { answer: 'Levi Strauss',
-      hint: 'Who invented jeans?',
-    },
-]
-
-// FASHION ANSWERS AND HINTS 
-const getRandomFashionAns = () => {
-    return fashionList[Math.floor(Math.random() * fashionList.length)]
-}
-getRandomFashionAns()
-
-// SHOWING THE HINTS 
 const hintButton = document.getElementById('hint')
 const hintContainer = document.getElementById('hintContainer')
+const answerContainer = document.getElementById('answerContainer')
+const underscoresContainer = document.getElementById('underscoresContainer')
+const userLivesDisplay = document.getElementById('userLives')
+const winCountDisplay = document.getElementById('winCount')
 
-hintButton.addEventListener('click', () => {
-    const {hint} = getRandomFashionAns()
-    hintContainer.innerText = ' ' + `${hint}`
-})
-
-// START THE GAME 
-
-
+// CREATING ALPHABETS 
